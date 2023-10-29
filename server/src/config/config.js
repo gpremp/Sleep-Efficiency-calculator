9dd98ef2a497527419b953181ddb9bf4259bc2ca
Prem Kumar Gupta <gpremp7736@gmail.com>
2023-10-29 16:45:53 +0530

    Upload Project

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,4 @@
- const DB = "mongodb+srv://gpremp7736:GameGame@cluster0.y56rpua.mongodb.net/sleep-info-database?retryWrites=true&w=majority"
- const JWTPRIVATEKEY = "jwtprivatekey001"
+const JWTPRIVATEKEY = "jwtprivatekey001"
 
 
-module.exports = { DB,JWTPRIVATEKEY};
+module.exports = {JWTPRIVATEKEY};
